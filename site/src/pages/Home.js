efa4097b4964832ba82d96dfbb08ac83618eac17
Mootbing <50122069+Mootbing@users.noreply.github.com>
2022-09-14 23:17:54 -0400

    lerp

--- site/src/pages/Home.js
+++ site/src/pages/Home.js
@@ -20,21 +20,23 @@ const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
 export default function Home(){
 
     const [scrollY, setScrollY] = useState(0);
+    const [done, setDone] = useState(false);
 
     let navigate = useNavigate();
 
-    window.addEventListener("scroll", function(){
+    const scrollHandler = () =>{
         setScrollY(window.scrollY);
 
-        // console.log(window.scrollY);
-
-        if (window.scrollY >= 1300 && !redirAlready)
+        if (!done && window.scrollY >= 1300)
         {
-            redirAlready = true;
+            // window.removeEventListener("scroll", scrollHandler);
+            setDone(true);
             console.log("over at " + window.scrollY);
             navigate("/about") 
         }
-    });
+    }
+
+    window.addEventListener("scroll", scrollHandler);
 
     //for scrolling
     const divRef = useRef(null);
@@ -45,7 +47,9 @@ export default function Home(){
     // const [rot, setRotation] = useState([0, 0, 0]);
 
     const animProps = useSpring({
-        rotation: scrollY >= 200 ? [0, 0, 0] : [0, Math.PI / 2, 0],
+        rotation: scrollY >= 200 ? [0, 0, 0] : [0, 0, 0],
+        scale: scrollY >= 100 ? [1 + (scrollY - 200)/3000 * 1, 1 + (scrollY - 200)/3000 * 1, 1 + (scrollY - 200)/3000 * 1] : [4, 4, 4],
+        position: scrollY >= 100 ? [0, 0, 0] : [0, -6, 0],
     })
 
     const recurAnim = async (tick = -100, callback = null) => {
@@ -72,20 +76,20 @@ export default function Home(){
         <div>
             {/* 0 at top 1 at y=200 */}
             <NavBar t={topBarOffset} background={"#000"}/>
-            <h1 className="big-center-header" style={{top: 750 +  50 + topBarOffset * 10, position: "absolute", textAlign: "center", width: "100%"}}>
+            <h1 className="big-center-header" style={{fontSize: clamp(200 - scrollY * 2, 1, 200) , position: "fixed", textAlign: "center", width: "100%", height: "100%", justifySelf: "center", color: "green", zIndex: 1}}>
                 {
                     TypeWriter(
                         [                            
                             // "ENGINEERS",
                             // "INNOVATORS",
                             // "BETTER THAN LARRY",
-                            "FTC21630",
-                            " "
+                            "FTC21630"
                         ],
                         true,
                         true,
                         () => {
                             recurAnim(-100);
+
                             // if (window.pageYOffset != 0) return;
                             // divRef.current.scrollIntoView()
                         },
@@ -95,7 +99,7 @@ export default function Home(){
                 }
             </h1>
             
-            <Canvas style={{position: "fixed", height: "100vh", width: "100%", top : -75 * topBarOffset + 120}}>
+            <Canvas style={{position: "fixed", height: "100vh", width: "100%", bottom : clamp(scrollY, -250, 100)}}>
                 <ambientLight intensity={1}/>
                 <directionalLight
                     castShadow
@@ -111,26 +115,24 @@ export default function Home(){
                     shadow-camera-top={-10}
                     shadow-camera-bottom={10}
                 />
+                {scrollY >= 100 && <group>
                 <pointLight position={[50, 0, 50]} intensity={1} />
                 <pointLight position={[-50, 0, 50]} intensity={1} />
+                </group>}
                 <Suspense>
-                    <a.group rotation={animProps.rotation}>
+                    <a.group position={animProps.position} rotation={animProps.rotation} scale={animProps.scale}>
                         <Computer/>
                     </a.group>
                 </Suspense>
                 <planeGeometry args={[100, 100]} />
 
-                <OrbitControls enableRotate={true} enableZoom={false} enablePan={false} />
+                {scrollY >= 500 && <OrbitControls enableRotate={true} enableZoom={false} enablePan={false} />}
             </Canvas>
 
             <div className="first-scroll-div" style={{
                 position: "absolute",
-                // top: 800,
-                // width: "50vw",
-                // position: clamp(scrollY - 400, -500, 25) >= 25 ? "fixed" : "absolute",
                 top: clamp(scrollY - 300, -1000, 0) * 3 + 500,
                 backgroundColor: "rgba(0, 0, 0, 0.15)",
-                // borderRadius: 25,
                 padding: 25
             }}>
                 <h1>Our Robot</h1>
@@ -140,35 +142,14 @@ export default function Home(){
 
             {/* <div className="first-scroll-div" style={{
                 position: "absolute",
-                top: 1200,
-                right: clamp(scrollY - 900, -700, 0),
-                backgroundColor: "rgba(222, 2, 2, 0.15)",
-                // borderRadius: 25,
+                top: clamp(scrollY - 300, -1000, 0) * 3 + 500,
+                right: 0,
+                backgroundColor: "rgba(0, 0, 0, 0.15)",
                 padding: 25
             }}>
-                <h1></h1>
-                <p>lorem ipusm asdlfjskjallfjdfskjjafsd</p>
+                <h1>Our Robot</h1>
+                <p>A 3D Rendering of our team's robot. <br/> <br/> <b>Drag to spin around.</b></p>
             </div> */}
-
-            {/* closing div */}
-            {/* <div style={{
-                position: "fixed",
-                // top: 1500,
-                right: clamp(scrollY - 1200, -1000, 0),
-                backgroundColor: "#000",
-                width: "50vw",
-                height: "100vh",
-                zIndex: -1,
-            }} /> */}
-
-            {/* <div style={{
-                position: "fixed",
-                // top: 1500,
-                left: clamp(scrollY - 1200, -1000, 0),
-                backgroundColor: "#000",
-                width: "50vw",
-                height: "100vh"
-            }} /> */}
 
             <div style={{
                 position: "fixed",
